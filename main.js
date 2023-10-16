130cba8cdfefa9ccefd90948f44f2af3abe741c5
Moon Rider <itachi0777@mail.ru>
2023-10-16 15:43:18 +0500

    Bankai Mukōjōchū Muramasa

---
+++ main.js
@@ -0,0 +1,36 @@
+// First task //
+
+// function firstTask(arg1, arg2, arg3, arg4, arg5) {
+//     console.log("Argument 1:", arg1);
+//     console.log("Argument 2:", arg2);
+//     console.log("Argument 3:", arg3);
+//     console.log("Argument 4:", arg4);
+//     console.log("Argument 5:", arg5);
+//   }
+  
+//   firstTask("Hello", 42, true, [1, 2, 3], { name: "John" });
+
+
+
+// Second task //
+
+// function secondTask() {
+//     const num1 = 10;
+//     const num2 = 20;
+//     const str1 = "Hello, ";
+//     const str2 = "world!";
+    
+//     return num1 + num2;     
+//     return num1 * num2;      
+//     return str1 + str2;         
+//     return num1 / num2;      
+//     return str2;               
+//   }
+  
+//   console.log(secondTask()); 
+  
+  
+  
+  
+  
+  
